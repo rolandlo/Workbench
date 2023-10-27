fb2df4ba9790ff5995edcab8bf79ce6f790943a2
Roland Lötscher <roland_loetscher@hotmail.com>
2023-10-27 13:26:46 +0200

    library: Port Menu Button demo to Python

---
+++ src/Library/demos/Menu Button/main.py
@@ -0,0 +1,16 @@
+import gi
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+from gi.repository import Gtk, Adw
+import workbench
+
+circular_switch : Adw.SwitchRow = workbench.builder.get_object("circular_switch")
+secondary_button : Gtk.MenuButton = workbench.builder.get_object("secondary")
+
+def notifyActive(switch, state):
+  if switch.get_active():
+    secondary_button.add_css_class("circular")
+  else:
+    secondary_button.remove_css_class("circular")
+
+circular_switch.connect("notify::active", notifyActive)
